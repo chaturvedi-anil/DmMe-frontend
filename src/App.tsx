f475ddfb68f73e5c8d71ee32660e7bc3f482388b
chaturvedi-anil <chaturvedia560@gmail.com>
2024-12-29 21:09:21 +0530

    Added room join and chat page

--- src/App.tsx
+++ src/App.tsx
@@ -19,26 +19,36 @@ const App = () => {
         },
       })
     );
-    inputRef.current.value = ""; // Clear input field after sending
+    inputRef.current.value = "";
   };
 
+  const joinChatRoom = () => {
+    if (!roomIdRef.current.value.trim()) {
+      return;
+    }
+
+    wsRef.current.send(
+      JSON.stringify({
+        type: "join",
+        payload: {
+          //@ts-ignore
+          roomId: roomIdRef.current?.value,
+        },
+      })
+    );
+
+    roomIdRef.current.value = "";
+
+  }
+  
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
      //@ts-ignore
     wsRef.current = ws;
 
     ws.onopen = () => {;
-      ws.send(
-        JSON.stringify({
-          type: "join",
-          payload: {
-            //@ts-ignore
-            roomId: roomIdRef.current.value,
-          },
-        })
-      );
+      alert("connected to server!");
     };
-
     ws.onmessage = (ev) => {      
        //@ts-ignore
       setMessages((m) => [...m, ev.data]);
@@ -55,21 +65,21 @@ const App = () => {
       <header className="mb-4">
         <h1 className="text-2xl font-bold text-center">DmMe - Real Time Chat Application</h1>
       </header>
-        <div className="w-full max-w-md">
-          <input
-            type="text"
-            ref={roomIdRef}
-            placeholder="Enter Room ID"
-            className="w-full px-4 py-2 mb-4 border rounded-md"
-          />
-          <button
-            onClick={() => {}}
-            className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-          >
-            Join Room
-          </button>
-        </div>
-        <main className="w-full max-w-md">
+      <div className="w-full max-w-md">
+        <input
+          type="text"
+          ref={roomIdRef}
+          placeholder="Enter Room ID"
+          className="w-full px-4 py-2 mb-4 border rounded-md"
+        />
+        <button
+          onClick={joinChatRoom}
+          className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Join Room
+        </button>
+      </div>
+        {/* <main className="w-full max-w-md">
           <div className="message-container h-64 bg-white border rounded-md overflow-y-scroll p-4 mb-4">
            
 
@@ -93,7 +103,7 @@ const App = () => {
               Send
             </button>
           </div>
-        </main>
+        </main> */}
     </div>
   );
 };
---
+++ src/pages/RoomJoinPage.tsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react'
+
+const roomJoinPage = () => {
+  const roomIdRef = useRef(null);
+  return (
+    <div>
+      <header className="mb-4">
+        <h1 className="text-2xl font-bold text-center">DmMe - Real Time Chat Application</h1>
+      </header>
+      <div className="w-full max-w-md">
+        <input
+          type="text"
+          ref={roomIdRef}
+          placeholder="Enter Room ID"
+          className="w-full px-4 py-2 mb-4 border rounded-md"
+        />
+        <button
+          onClick={joinChatRoom}
+          className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Join Room
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default roomJoinPage
